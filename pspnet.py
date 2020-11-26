8782a22bda2bee33f8c05b04b81d5811b5141521
Hannah_Haensen <hannah.schieber@hotmail.com>
2020-11-26 19:34:11 +0100

    Update pspnet.py
    added main for testing purpose

--- pspnet.py
+++ pspnet.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-import extractors
+from model import extractor
 
 
 class PSPModule(nn.Module):
@@ -20,7 +20,7 @@ class PSPModule(nn.Module):
 
     def forward(self, feats):
         h, w = feats.size(2), feats.size(3)
-        priors = [F.upsample(input=stage(feats), size=(h, w), mode='bilinear') for stage in self.stages] + [feats]
+        priors = [F.upsample(input=stage(feats), size=(h, w), mode='bilinear', align_corners=True) for stage in self.stages] + [feats]
         bottle = self.bottleneck(torch.cat(priors, 1))
         return self.relu(bottle)
 
@@ -44,7 +44,7 @@ class PSPNet(nn.Module):
     def __init__(self, n_classes=18, sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet34',
                  pretrained=True):
         super().__init__()
-        self.feats = getattr(extractors, backend)(pretrained)
+        self.feats = getattr(extractor, backend)(pretrained, n_classes)
         self.psp = PSPModule(psp_size, 1024, sizes)
         self.drop_1 = nn.Dropout2d(p=0.3)
 
@@ -53,19 +53,27 @@ class PSPNet(nn.Module):
         self.up_3 = PSPUpsample(64, 64)
 
         self.drop_2 = nn.Dropout2d(p=0.15)
+
         self.final = nn.Sequential(
             nn.Conv2d(64, n_classes, kernel_size=1),
-            nn.LogSoftmax()
         )
 
-        self.classifier = nn.Sequential(
-            nn.Linear(deep_features_size, 256),
-            nn.ReLU(),
-            nn.Linear(256, n_classes)
+        self.aux = nn.Sequential(
+            nn.Conv2d(1024, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.15),
+            nn.Conv2d(256, n_classes, kernel_size=1)
         )
 
     def forward(self, x):
-        f, class_f = self.feats(x) 
+        x_size = x.size()
+        # assert (x_size[2]-1) % 8 == 0 and (x_size[3]-1) % 8 == 0
+        h = x_size[2]
+        w = x_size[3]
+
+        f, class_f = self.feats(x)
+
         p = self.psp(f)
         p = self.drop_1(p)
 
@@ -78,6 +86,21 @@ class PSPNet(nn.Module):
         p = self.up_3(p)
         p = self.drop_2(p)
 
-        auxiliary = F.adaptive_max_pool2d(input=class_f, output_size=(1, 1)).view(-1, class_f.size(1))
+        p = self.final(p)
 
-        return self.final(p), self.classifier(auxiliary)
+        p = F.interpolate(p, size=(h, w), mode='bilinear', align_corners=True)
+        # aux branch
+        aux = self.aux(class_f)
+
+        aux = F.interpolate(aux, size=(h, w), mode='bilinear', align_corners=True)
+
+        return p, aux
+
+if __name__ == '__main__':
+    input = torch.rand(4, 3, 64, 64)
+    model = PSPNet(n_classes=19, pretrained=False, sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet50')
+    model.eval()
+    print(model)
+    output = model(input)
+    print('PSPNet', output[0].size())
+    print('PSPNet', output[1].size())
